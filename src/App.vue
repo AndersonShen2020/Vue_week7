b3b16109671094baf9a7dc480bb5e404baea701c
AndersonShen <shen.andersonshen@gmail.com>
2022-01-23 17:29:25 +0800

    調整 Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,54 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Coffee 專賣店</a>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#/about">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">購物車</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#/Login">登入</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <router-view />
-  <div class="btn btn-primary">AAA</div>
 </template>
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
+// @import "~bootstrap-icons/font/bootstrap-icons.scss";
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
 
-#nav {
-  padding: 30px;
+// #nav {
+//   padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
