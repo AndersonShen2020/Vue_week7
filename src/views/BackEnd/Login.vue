21c5e7b049fea40e3f09479136f85a4b58baa74f
AndersonShen <shen.andersonshen@gmail.com>
2022-01-24 15:13:18 +0800

    1. 安裝 axios 2. 測試登入帳號

--- src/views/BackEnd/Login.vue
+++ src/views/BackEnd/Login.vue
@@ -8,23 +8,64 @@
           class="form-control"
           id="floatingInput"
           placeholder="name@example.com"
+          v-model="user.username"
+          required
+          autofocus
         />
         <label for="floatingInput">請輸入管理員信箱</label>
       </div>
       <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+        <input
+          type="password"
+          class="form-control"
+          id="floatingPassword"
+          v-model="user.password"
+          placeholder="Password"
+        />
         <label for="floatingPassword">請輸入密碼</label>
       </div>
       <div class="form-check my-3">
         <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
         <label class="form-check-label" for="flexCheckChecked"> 記住我 </label>
       </div>
-      <button type="submit" class="btn btn-lg btn-primary w-100 mt-3">登入</button>
+      <button type="submit" class="btn btn-lg btn-primary w-100 mt-3" @click.prevent="login">
+        登入
+      </button>
     </form>
   </div>
 </template>
 <script>
-export default {};
+// import router from "../router";
+import axios from "axios";
+const url = "https://vue3-course-api.hexschool.io/v2"; // 請加入站點
+// const path = "ashen"; // 請加入個人 API path
+
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post(`${url}/admin/signin`, this.user)
+        .then((res) => {
+          console.log(res);
+          // expired = unix timestamp
+          const { token, expired } = res.data;
+          document.cookie = `hexToken=${token}; expires=${new Date(expired)};`;
+          // router.push("/products");
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+  },
+};
 </script>
 <style lang="scss">
 .formlogin {
