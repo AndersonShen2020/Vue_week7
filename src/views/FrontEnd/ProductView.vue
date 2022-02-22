30a6cc344e121a014084e1f74dbcf0194ada0904
AndersonShen <shen.andersonshen@gmail.com>
2022-02-23 01:53:52 +0800

    add: 單一產品頁面經由動態路由載入

---
+++ src/views/FrontEnd/ProductView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>單一產品頁面</h1>
+    {{ Item }}
+  </div>
+</template>
+<script>
+import axios from "axios";
+const url = process.env.VUE_APP_API; // 請加入站點
+const path = process.env.VUE_APP_PATH; // 請加入個人 API path
+
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      Item: {},
+    };
+  },
+  mounted() {
+    axios.get(`${url}/api/${path}/product/${this.id}`).then((res) => {
+      console.log(res);
+      this.Item = res;
+    });
+  },
+};
+</script>
+<style></style>
