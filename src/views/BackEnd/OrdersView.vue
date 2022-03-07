14055890982e55710830b60784dc10a296060e17
AndersonShen <shen.andersonshen@gmail.com>
2022-03-07 15:40:34 +0800

    fix: 增加後台訂單分頁功能

--- src/views/BackEnd/OrdersView.vue
+++ src/views/BackEnd/OrdersView.vue
@@ -49,22 +49,42 @@
       </template>
     </tbody>
   </table>
+  <pagination :pages="pagination" @emitpages="getOrders"></pagination>
 </template>
+
 <script>
 import axios from "axios";
-const urlPath = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/orders`;
+
+// component
+import pagination from "@/components/common/pagination.vue";
 
 export default {
+  components: {
+    pagination,
+  },
   data() {
     return {
+      pagination: null,
       orders: [],
     };
   },
+  methods: {
+    async getOrders(page = 1) {
+      const urlPath = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/admin/orders?page=${page}`;
+      await axios
+        .get(urlPath)
+        .then((res) => {
+          console.log(res);
+          this.orders = res.data.orders;
+          this.pagination = res.data.pagination;
+        })
+        .catch((err) => {
+          console.dir(err.response);
+        });
+    },
+  },
   mounted() {
-    axios.get(urlPath).then((res) => {
-      console.log(res);
-      this.orders = res.data.orders;
-    });
+    this.getOrders();
   },
 };
 </script>
